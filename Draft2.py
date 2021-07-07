d1220e932840a019644d3b0d9b6af138f0ddfb65
a_6x <abdullah.aaz2007@gmail.com>
2021-07-07 11:28:42 +0300

    Made for no reason

---
+++ Draft2.py
@@ -0,0 +1,8 @@
+def Welcome-Screen
+
+print('Hello Client Welcome to the no purpose login please choose from the menu.')
+print('1.Log in to your account')
+print('2.Sign up a new account')
+print('3.Exit')
+entance = input()
+#To complete the program you need to reeach level: Function
