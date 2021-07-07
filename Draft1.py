a4c61ed6a2fc4f0d9eac798b7c8b0c952ed99c06
a_6x <abdullah.aaz2007@gmail.com>
2021-07-07 03:30:34 +0300

    No more Emoji ;(

--- Draft1.py
+++ Draft1.py
@@ -1,4 +1,4 @@
-print("Give me any text to spell it out vertically 😎") #Asks for a text
+print("Give me any text to spell it out vertically ") #Asks for a text
 Word = input() #input() is to let the client to add the text
 print(type(Word)) #to make sure that every input is string
 for i in range(0, len(Word)): #for var:i in the range 0 - length of var:Word
