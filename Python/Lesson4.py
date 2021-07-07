616d53c0a4e998417e6da17787ecdf4efd6c6e49
a_6x <abdullah.aaz2007@gmail.com>
2021-07-07 13:45:15 +0300

    New Lesson

---
+++ Python/Lesson4.py
@@ -0,0 +1,29 @@
+from time import sleep
+print("#####  Operators   ######")
+
+print("####  Logical  ####")
+Variable1 = True
+Variable2 = False
+print(Variable1 and Variable2)
+print(Variable1 or Variable2)
+print(Variable1 != Variable2 )
+print(Variable1 == Variable2)
+print(not Variable1)
+
+print("####  Unary  ####")
+Variable1 = 5
+Variable1 = -Variable1
+print(Variable1)
+
+Variable1 = -Variable1 * 2
+print(Variable1)
+
+print("####  Relational  ####")
+
+Variable2 = 4
+print(Variable1 < Variable2)
+print(Variable1 > Variable2)
+print(Variable1 <= Variable2)
+print(Variable1 >= Variable2)
+print(Variable1 == Variable2)
+print(Variable1 != Variable2)
