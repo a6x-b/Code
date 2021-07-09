89be41e1b8896cbc710a44fec8c8625753763c60
a_6x <abdullah.aaz2007@gmail.com>
2021-07-09 04:28:33 +0300

    Small Changes

--- Python/Vertical speller.py
+++ Python/Vertical speller.py
@@ -1,6 +1,6 @@
 print("Give me any text to spell it out vertically ") #Asks for a text
 Word = input() #input() is to let the client to add the text
 print(type(Word)) #to make sure that every input is string
-for i in range(0, len(Word)): #for var:i in the range 0 - length of var:Word
-    print(Word[i])
+for i in (Word): #for var:i in the range 0 - length of var:Word
+    print(i)
 #this was made with lesson1.py
