32a4bf92345ad40c6c2ce760eb7493b07f0093a0
a_6x <abdullah.aaz2007@gmail.com>
2021-07-12 18:27:30 +0300

    tuwaiq

--- Python/Vertical-speller-SA.py
+++ Python/Vertical-speller-SA.py
@@ -3,4 +3,6 @@ j = 0
 while j < 20:
     for i in 'Sus Amogus':
         print(i)
-j+=1
+    j+=1
+from webbrowser import open
+open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
---
+++ Python/s.py
@@ -0,0 +1,6 @@
+def concat_names(name1, name2):
+    # write your code here
+    name1,name2 = str(name1), str(name2)
+    Name = name1 + name2
+    print(Name)
+concat_names('Hello ', 'World')
