fb6af2519ed88ae22319eb5734c1d08c2b0d312a
a_6x <abdullah.aaz2007@gmail.com>
2021-07-19 23:29:30 +0300

    IDK2

--- Python/GUI/Butt-on.py
+++ Python/GUI/Butt-on.py
@@ -7,6 +7,8 @@ def DaClick():
         Warning = Label(GUI, text=f'{5-i}')
         Warning.pack()
         sleep(1)
-    system('shutdown  /s /t 1')
-DaBooton = Button(GUI, text='Click ME!',command=DaClick(),fg='Blue',bg='Yellow')
+    
+DaBooton = Button(GUI, text='Click ME!',fg='Blue',bg='Yellow',command=DaClick())
 DaBooton.pack()
+
+GUI.mainloop()
