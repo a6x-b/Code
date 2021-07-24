cdecb45c676e20691a3c42feecef1bd0d3aea72f
a_6x <abdullah.aaz2007@gmail.com>
2021-07-24 12:37:23 +0300

    Cases

---
+++ Javascript/Cases-and-statments4.js
@@ -0,0 +1,17 @@
+let Names = ['Abdullah','Atyah','Ghena','Amogus'];
+let selector = 1;
+switch(selector){
+    default:
+        console.log(Names[selector]);
+    case 1:
+    case 2:
+        console.log(Names);
+};
+if (true){
+    console.log('This is true')
+} else if (false){
+    console.log('this is false')
+}
+else{
+    console.log('this is impossible')
+}
