9f7039c0a9c8780f9049969e0ddeb42a42708808
a_6x <abdullah.aaz2007@gmail.com>
2021-08-10 11:31:52 +0300

    102 2

--- Javascript/102/Nums2.js
+++ Javascript/102/Nums2.js
@@ -36,4 +36,29 @@ console.log(`Example: 69.42 with trunc is ${Math.trunc(69.42)}`); //oops i calle
 console.log("Also Number.isInteger() returns boolean value")
 const Nomb = 5;
 const IsInt = Number.isInteger(Nomb);
-console.log(`Is ${Nomb} an Intger? The answer is ${IsInt}.`);
+if (isNaN(Nomb)) {
+    console.log(`${Nomb} is not a number`)
+} else{
+    console.log(`Is ${Nomb} an Intger? The answer is ${IsInt}.`);
+}
+pows = 3;
+console.log(`${Nomb} power of ${pows} = ${Nomb**pows}.`);
+console.log(`The square root of ${Nomb**pows} = ${Math.sqrt(Nomb**pows)}`);
+if (Math.sign(Nomb) == 1) {
+    console.log(`${Nomb} is positive.`)
+} else if (Math.sign(Nomb) == -1) {
+    console.log(`${Nomb} is negative.`)
+} else {
+    console.log(`0 is zero`)
+}
+console.log(1/0);
+console.log(-1/0);
+console.log(`Random number 0-1 -> ${Math.random()}`);
+console.log(69_420);
+let Decimal = 15; //decimal
+let Hexadecimal = 0xF; //Hexadecimal
+let Octal = 0o17; //octal
+let Binary = 0b01111; //binary
+let Numberic_System = [Decimal,Hexadecimal,Octal, Binary]    
+for (let i in Numberic_System) {
+    console.log(Numberic_System[i])};
