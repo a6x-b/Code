233b1e8ba15e7bf4f01d7f90ef0f9a40b6b4bdc6
a6x-b <abdullah.aaz2007@icloud.com>
2021-10-08 13:15:20 +0300

    IDK

---
+++ Project/Numberic_System_Conv/Converter.py
@@ -0,0 +1,12 @@
+Decimals = ['0','1','2','3','4','5','6','7','8','9']
+HexaDecimals = Decimals + ['A','B','C','D','E','F']
+#input reciever
+def The_input():
+    Number = input('Please input a number. ')
+    #Removing spaces
+    Number = Number.replace(' ','')
+    return Number
+
+#Numeric system detector
+def Number_Detector():
+    pass
