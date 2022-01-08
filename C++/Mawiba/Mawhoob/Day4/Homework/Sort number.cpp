10fb80e93566331548e324649d4910f78c84165a
a6x-b <abdullah.aaz2007@icloud.com>
2022-01-08 08:14:11 +0300

    Winter Day1

--- C++/Mawiba/Mawhoob/Day4/Homework/Sort number.cpp
+++ C++/Mawiba/Mawhoob/Day4/Homework/Sort number.cpp
@@ -4,16 +4,16 @@ int main()
 {
 long long a,b,c;
 cin>> a >>b >> c;
-if (a>=b and b>=c)
+if (a>=b && b>=c)
     cout << c <<endl << b << endl<< a <<endl<<endl << a <<endl << b << endl<< c <<endl;
-else  if (c>=b and b>=a)
+else  if (c>=b && b>=a)
     cout << a <<endl << b << endl<< c <<endl<<endl << a <<endl << b << endl<< c <<endl;
-else if (a>=c and c>=b)
+else if (a>=c && c>=b)
     cout << b <<endl << c << endl<< a <<endl<<endl << a <<endl << b << endl<< c <<endl;
-else  if (b>=c and c>=a)
+else  if (b>=c && c>=a)
     cout << a <<endl <<c << endl<< b <<endl<<endl << a <<endl << b << endl<< c <<endl;
-else if (b>=a and a>=c)
+else if (b>=a && a>=c)
     cout << c <<endl << a << endl<< b <<endl<<endl << a <<endl << b << endl<< c <<endl;
-else if (c>=a and a>=b)
+else if (c>=a && a>=b)
     cout << b <<endl << a << endl<< c <<endl<<endl << a <<endl << b << endl<< c <<endl;
 }
---
+++ C++/Mawiba/Winter/Day1/Float_or_int.cpp
@@ -0,0 +1,2 @@
+#include <iostream>
+using namespace std;int main(){float N;cin >> N;if (N/1 != int(N)){cout << "float " << N;}else cout<<"int "<<N;}
