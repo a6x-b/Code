a4e51310a9ae4e49949cd9f9d464ce4764c4ffe1
a6x-b <abdullah.aaz2007@icloud.com>
2022-01-08 09:18:22 +0300

    Winter Day1

--- C++/Mawiba/Mawhoob/Day6/Triangle_loop.cpp
+++ C++/Mawiba/Mawhoob/Day6/Triangle_loop.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main()
 {
-    int Base,Counter;
+    int Base,Counter = 0;
     cout<<"Enter the base ";
     cin>>Base;
     while (Counter<Base)
     {
         Counter++;
-        for (int Stars = 0; Stars <= Counter; Stars++)
+        for (int Stars = 0; Stars < Counter; Stars++)
     {
         cout<<"*";
     }
