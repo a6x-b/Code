6505dd2c97e31a4ca6a0b00d977f7879a5839bd5
a6x-b <abdullah.aaz2007@icloud.com>
2023-05-28 17:54:18 +0300

    🐞 fix: Lag + permissions
    Fixed the problem when The World reset permissions to default instead of their previous state. and fixed the lag and high ping when the bot joins by disabling the block 57-85

--- Javascript/ZaWarudo/commands/stando/stop_time.js
+++ Javascript/ZaWarudo/commands/stando/stop_time.js
@@ -1,7 +1,9 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, BitField, VoiceChannel } = require('discord.js');
 let Global_Vars = require('../../Global_Vars.js');
 const { createAudioResource } = require('@discordjs/voice');
 const { joinVoiceChannel } = require('@discordjs/voice');
+const { VoiceConnectionStatus } = require('@discordjs/voice');
+const { PermissionsBitField } = require('discord.js');
 const channels = [];
 
 module.exports = {
@@ -16,6 +18,9 @@ module.exports = {
 
 		const worldInvader = interaction.channel.guild.roles.cache.find(r => r.name === 'World Invader');
 		const ZaWarudo = interaction.channel.guild.roles.cache.find(r => r.name === 'The World');
+		const EVERYONE =  interaction.channel.guild.roles.everyone;
+		if (!worldInvader) console.log('[WARN] the role "World Invader" is not found');
+		if (!ZaWarudo) console.log('[WARN] the role "The World" is not found');
 		if (interaction.member.roles.cache.some((r) => r.name === "The World")) {
 			if (Global_Vars.TimeStopped) {
 				console.error("[WARN] blud tried to stop time, while it's already is.");
@@ -26,16 +31,30 @@ module.exports = {
 			await interaction.reply({content:'https://tenor.com/view/diego-brando-diego-alternate-the-world-za-warudo-gif-17597363', ephemeral: true});
 			Global_Vars.TimeStopped = true;
 			// console.log(`[DEV] allChannels = ${Global_Vars.allChannels}`);
+			const viewChannelPermissions = [];
 			if (Global_Vars.allChannels) {
-				
 				interaction.guild.channels.cache.forEach((channel) => {
 					if (channel.isTextBased())
 					channels.push(channel);
+					if (channel.permissionOverwrites.cache.find(role => role.id === EVERYONE.id).allow.any(PermissionsBitField.Flags.ViewChannel)) {
+						viewChannelPermissions.push(true);
+					} else if (channel.permissionOverwrites.cache.find(role => role.id === EVERYONE.id).deny.any(PermissionsBitField.Flags.ViewChannel)) {
+						viewChannelPermissions.push(false);
+					} else {
+						viewChannelPermissions.push(null);
+					}
 					channel.permissionOverwrites.create(channel.guild.roles.everyone, { ViewChannel: false });
 					channel.permissionOverwrites.create(worldInvader, { SendMessages: false, ViewChannel: true });
 					channel.permissionOverwrites.create(ZaWarudo, { SendMessages: true, ViewChannel: true });
 				});
 			} else {
+				if (interaction.channel.permissionOverwrites.cache.find(role => role.id === EVERYONE.id).allow.any(PermissionsBitField.Flags.ViewChannel)) {
+					viewChannelPermissions.push(true);
+				} else if (interaction.channel.permissionOverwrites.cache.find(role => role.id === EVERYONE.id).deny.any(PermissionsBitField.Flags.ViewChannel)) {
+					viewChannelPermissions.push(false);
+				} else {
+					viewChannelPermissions.push(null);
+				}
 				interaction.channel.permissionOverwrites.create(ZaWarudo, { SendMessages: true, ViewChannel: true });
 				interaction.channel.permissionOverwrites.create(interaction.channel.guild.roles.everyone, { ViewChannel: false });
 				interaction.channel.permissionOverwrites.create(worldInvader, { SendMessages: false, ViewChannel: true });
@@ -47,14 +66,15 @@ module.exports = {
 					guildId: interaction.member.voice.channel.guild.id,
 					adapterCreator: interaction.member.voice.channel.guild.voiceAdapterCreator,
 				});
+				
 				try {
 					connection.subscribe(audioPlayer);
 				} catch (error) {
 					console.error(error);
 				}
 				
-				// IDK, Code I found for checking the connection.
-				async () => {
+				// IDK, Code I found for checking the connection. (IT WAS THE SOURCE PROBLEM OF THE SOUND LAG)
+				/*async () => {
 					try {
 						return connection;
 					} catch (error) {
@@ -62,21 +82,23 @@ module.exports = {
 						connection.destroy();
 						console.error(error);
 					}
-				}
-				//*/
-				try {
-					TS = createAudioResource('./audio/ZaWarudoDiegoSFX.mp3');
-					audioPlayer.play(TS);
-					setTimeout( () => {audioPlayer.pause()}, 3000);
-				} catch (error) {
-					// await interaction.reply({content:'There was a problem in playing the audio.',ephemeral:true})
-					console.error(error);
-					console.error("[ERROR] Didn't play Time Stop audio");
-				}
+				}//*/
+				connection.on(VoiceConnectionStatus.Ready, () => { 
+					//*/
+					try {
+						TS = createAudioResource('./audio/ZaWarudoDiegoSFX.mp3');
+						audioPlayer.play(TS);
+						setTimeout( () => {audioPlayer.pause()}, 3000);
+					} catch (error) {
+						// await interaction.reply({content:'There was a problem in playing the audio.',ephemeral:true})
+						console.error(error);
+						console.error("[ERROR] Didn't play Time Stop audio");
+					}
+				});
 			}
 			if (Global_Vars.allChannels) {
 				interaction.guild.channels.cache.each(channel => {
-					if (channel.type === 2) {
+					if (channel.isVoiceBased()) {
 						const members = channel.members;
 						members.forEach(member => {
 							if (!member.user.bot) {
@@ -107,7 +129,8 @@ module.exports = {
 			}
 				console.log('[INFO] Time has stopped!!');
 				
-				await interaction.channel.send("Time has stopped.").then((msg) => {setTimeout(() => msg.delete(), 3500);});
+				await interaction.channel.send("Za Warudo!").then((msg) => {setTimeout(() => msg.delete(), 3500);});
+				// await interaction.channel.send("Time has stopped.").then((msg) => {setTimeout(() => msg.delete(), 3500);});
 				
 				//Time Resuming
 				setTimeout(() => {
@@ -128,13 +151,15 @@ module.exports = {
 					
 					// Checks if it's all channel or just one
 					if (Global_Vars.allChannels) {
+						let i = 0;
 						channels.forEach((channel) => {
-							channel.permissionOverwrites.create(channel.guild.roles.everyone, { ViewChannel: null });
+							channel.permissionOverwrites.create(channel.guild.roles.everyone, { ViewChannel: viewChannelPermissions[i] });
 							channel.permissionOverwrites.delete(worldInvader, 'Time Resumed');
 							channel.permissionOverwrites.delete(ZaWarudo, 'Time Resumed');
+							i = i + 1
 						});
 					} else {
-						interaction.channel.permissionOverwrites.edit(interaction.channel.guild.roles.everyone, { ViewChannel: null });
+						interaction.channel.permissionOverwrites.edit(interaction.channel.guild.roles.everyone, { ViewChannel: viewChannelPermissions[0] });
 						interaction.channel.permissionOverwrites.delete(worldInvader, 'Time Resumed');
 						interaction.channel.permissionOverwrites.delete(ZaWarudo, 'Time Resumed');
 					}
@@ -147,7 +172,6 @@ module.exports = {
 					if (members) {
 						members.forEach(member => {
 							if (!member.user.bot) {
-								
 								if (!member.roles.cache.some((r) => r.name === "The World")) {
 									member.voice.setMute(false);
 									if (!member.roles.cache.some((r) => r.name === "World Invader")) {
