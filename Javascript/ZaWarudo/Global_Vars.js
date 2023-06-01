a3e845a9f43592d06b1141da0566bf40e3232aef
a6x-b <abdullah.aaz2007@icloud.com>
2023-06-01 20:11:07 +0300

    ✨ feat: Variables changes
    Renamed Time_STOP Duration and Globalized all variables

--- Javascript/ZaWarudo/Global_Vars.js
+++ Javascript/ZaWarudo/Global_Vars.js
@@ -1,15 +1,16 @@
 // const options = require('./commands/stando/options');
 const options = require('./options.json');
 const fs = require('node:fs');
-// console.log(settings);
-global.Time_STOP = options.Time_STOP;
-// let TimeStopped = false;
-global.allChannels = options.allChannels;
 
-const saveSettings = () => {
+global.Duration = options.Duration;
+global.allChannels = options.allChannels;
+global.character = options.character;
+global.TimeStopped = false
+
+global.saveSettings = () => {
     // console.log(fs.readFile('./options.json'));
-        // console.log(this.Time_STOP);
-        fs.writeFile("options.json", `{ "Time_STOP": ${this.Time_STOP}, "allChannels": ${this.allChannels} } `, {
+        // console.log(this.Duration);
+        fs.writeFile("options.json", `{ "Duration": ${Duration}, "allChannels": ${allChannels}, "character": "${character}" }`, {
             encoding: "utf8",
             flag: "w",
             mode: 0o666
@@ -17,17 +18,7 @@ const saveSettings = () => {
         (err) => {
         if (err)
             console.log(err);
-        else {
-        // console.log("File written successfully\n");
-        // console.log("The written has the following contents:");
-        // console.log(fs.readFileSync("options.json", "utf8"));
-    }
 });
-    // console.log(fs.readFile('./options.json'));
 }
-exports.saveSettings = saveSettings;//*/
-exports.Time_STOP = Time_STOP;
-exports.TimeStopped = false;
-exports.allChannels = allChannels;
 
-// module.exports = { Time_STOP: Time_STOP, TimeStopped: TimeStopped, allChannels: allChannels };
+// module.exports = { Duration: Duration, TimeStopped: TimeStopped, allChannels: allChannels };
